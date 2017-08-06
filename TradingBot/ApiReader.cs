e8e5f250f07575c3b09dbc93f3653914de16b0aa
Matt Beton <matthew.beton@gmail.com>
2017-08-06 14:27:11 +0100

    Changed ApiConnector to ApiReader
    For new class ApiConnector, inhereting ApiReader. Allows readonly connectors to be created.

---
+++ TradingBot/ApiReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TradingBot
+{
+	public class ApiReader
+	{
+		public ApiReader()
+		{
+		}
+
+		public TickerResult GetTickerResult(CurrencyPair currency)
+		{
+			string rawData = DownloadString("https://api.bitfinex.com/v2/ticker/" + currency.GetBitfinexCurrencyPair());
+
+			TickerResult result = JsonConvert.DeserializeObject<TickerResult>(rawData);
+
+			return result;
+		}
+		
+		public static string DownloadString(string address)
+		{
+			WebClient client = new WebClient();
+			string reply = client.DownloadString(address);
+
+			return reply;
+		}
+	}
+}
