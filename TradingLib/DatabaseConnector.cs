8578b22f017220a5c07d693935bfda2822f4cf2a
Matt Beton <matthew.beton@gmail.com>
2017-08-06 17:29:07 +0100

    Started DatabaseConnector
    Added SQLiteConnection and InsertIntoDatabase

--- TradingLib/DatabaseConnector.cs
+++ TradingLib/DatabaseConnector.cs
@@ -1,11 +1,28 @@
-﻿using System.Data.SQLite;
+﻿using System;
+using System.Data.SQLite;
 
 namespace TradingLib
 {
 	public class DatabaseConnector
 	{
+		SQLiteConnection connection = new SQLiteConnection(
+			@"Data Source=C:\Users\matth\OneDrive\Documents\Visual Studio 2017\Projects\TradingBot\priceData.db;");
+
 		public DatabaseConnector()
 		{
+			connection.Open();
+		}
+
+		public void InsertIntoDatabase(int date, double price, double volBid, double volAsk)
+		{
+			string sql = "INSERT INTO prices VALUES (" + 
+				Convert.ToString(date) + ", " + 
+				Convert.ToString(price) + ", " + 
+				Convert.ToString(volBid) + ", " + 
+				Convert.ToString(volAsk) + ");";
+
+			SQLiteCommand command = new SQLiteCommand(sql, connection);
+			command.ExecuteNonQuery();
 		}
 	}
 }
