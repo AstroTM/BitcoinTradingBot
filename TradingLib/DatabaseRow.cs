d1f603dbba27ccd3e975aebd71da63bc509a86cf
Matt Beton <matthew.beton@gmail.com>
2017-08-06 18:11:10 +0100

    Created DatabaseRow class
    Holds info from a row of the database.

---
+++ TradingLib/DatabaseRow.cs
@@ -0,0 +1,18 @@
+﻿namespace TradingLib
+{
+	public class DatabaseRow
+	{
+		public int date;
+		public double price;
+		public double volBid;
+		public double volAsk;
+
+		public DatabaseRow(int date, double price, double volBid, double volAsk)
+		{
+			this.date = date;
+			this.price = price;
+			this.volBid = volBid;
+			this.volAsk = volAsk;
+		}
+	}
+}
