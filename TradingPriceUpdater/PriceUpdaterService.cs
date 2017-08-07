34fdf8d7e1dfeb5b53f79635ee53bdcdfdbad020
Matt Beton <matthew.beton@gmail.com>
2017-08-07 13:11:45 +0100

    Added ApiReader and DatabaseConnector to service
    Class level variables, inialised in OnStart

--- TradingPriceUpdater/PriceUpdaterService.cs
+++ TradingPriceUpdater/PriceUpdaterService.cs
@@ -8,12 +8,15 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using TradingLib;
 
 namespace TradingPriceUpdater
 {
 	public partial class PriceUpdaterService : ServiceBase
 	{
 		private Timer _timer;
+		private ApiReader apiRead;
+		private DatabaseConnector databaseConnect;
 
 		public PriceUpdaterService()
 		{
@@ -24,7 +27,9 @@ namespace TradingPriceUpdater
 		{
 			_timer = new Timer(10 * 1000); // every 10 seconds
 			_timer.Elapsed += new System.Timers.ElapsedEventHandler(TimerElapsed);
-			_timer.Start(); 
+			_timer.Start();
+			apiRead = new ApiReader();
+			databaseConnect = new DatabaseConnector();
 		}
 
 		protected override void OnStop()
@@ -33,7 +38,7 @@ namespace TradingPriceUpdater
 
 		public void TimerElapsed(object sender, System.Timers.ElapsedEventArgs e)
 		{
-			
+
 		}
 	}
 }
