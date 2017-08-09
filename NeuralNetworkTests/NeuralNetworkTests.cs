c4b3d4bf533974358380af8d9ae348c777521e23
Matt Beton <matthew.beton@gmail.com>
2017-08-09 21:20:15 +0100

    Created unit testing project for NeuralNetwork

---
+++ NeuralNetworkTests/NeuralNetworkTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TradingBot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingBot.Tests
+{
+	[TestClass()]
+	public class NeuralNetworkTests
+	{
+		private NeuralNetwork ANN = new NeuralNetwork();
+
+		[TestMethod()]
+		public void TrainNetworkTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
