c49ea8da00bec38291ba6b9cd64c0a7e7851052d
Matt Beton <matthew.beton@gmail.com>
2017-08-09 09:17:39 +0100

    Removed the project 'NeuralNetwork'

--- TradingBot/NeuralNetwork.py
+++ TradingBot/NeuralNetwork.py
@@ -18,22 +18,6 @@ def forwardprop(X, w_1, w_2):
     yhat = tf.matmul(h, w_2)  # The \varphi function
     return yhat
 
-def get_iris_data():
-    """ Read the iris data set and split them into training and test sets """
-    iris   = datasets.load_iris()
-    data   = iris["data"]
-    target = iris["target"]
-
-    # Prepend the column of 1s for bias
-    N, M  = data.shape
-    all_X = np.ones((N, M + 1))
-    all_X[:, 1:] = data
-
-    # Convert into one-hot vectors
-    num_labels = len(np.unique(target))
-    all_Y = np.eye(num_labels)[target]  # One liner trick!
-    return train_test_split(all_X, all_Y, test_size=0.33, random_state=RANDOM_SEED)
-
 def main():
     train_X, test_X, train_y, test_y = get_iris_data()
 
