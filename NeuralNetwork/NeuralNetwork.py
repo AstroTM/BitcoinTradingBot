e40e6b3ff4db0d5bb1d19edc375976292eae6bbd
Matt Beton <matthew.beton@gmail.com>
2017-08-09 08:58:33 +0100

    Added forwardpropogation function

--- NeuralNetwork/NeuralNetwork.py
+++ NeuralNetwork/NeuralNetwork.py
@@ -1,4 +1,4 @@
-import tensorflow as tp
+import tensorflow as tf
 import numpy as np
 
 RANDOM_SEED = 147
@@ -8,3 +8,12 @@ def init_weights(shape):
     """ Weight initialization """
     weights = tf.random_normal(shape, stddev=0.1)
     return tf.Variable(weights)
+
+def forwardprop(X, w_1, w_2):
+    """
+    Forward-propagation.
+    IMPORTANT: yhat is not softmax since TensorFlow's softmax_cross_entropy_with_logits() does that internally.
+    """
+    h    = tf.nn.sigmoid(tf.matmul(X, w_1))  # The \sigma function
+    yhat = tf.matmul(h, w_2)  # The \varphi function
+    return yhat
