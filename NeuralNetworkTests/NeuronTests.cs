c4e10d3d05e8b3cf3dbcff2b7299b3e06014f441
Matt Beton <matthew.beton@gmail.com>
2017-08-12 12:13:57 +0100

    Fixed Neuron tests

--- NeuralNetworkTests/NeuronTests.cs
+++ NeuralNetworkTests/NeuronTests.cs
@@ -23,7 +23,7 @@ namespace TradingBot.Tests
 			double[] input = { 1.0, 2.0, 3.0, 4.0, 1.0, 2.0, 3.0 };
 			double[] expectedOutput = { 0.024, 0.064, 0.175, 0.475, 0.024, 0.064, 0.175 };
 
-			double[] output = HiddenNeuron.Softmax(input);
+			double[] output = OutputNeuron.Softmax(input);
 
 			for (int i = 0; i < output.Length; i++)
 			{
