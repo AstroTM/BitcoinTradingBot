66153d867584f5c521d9e000c6ac3acdc2d0b333
Matt Beton <matthew.beton@gmail.com>
2017-08-13 17:43:45 +0100

    Changed DatabaseRow to facilitate all 12 rows

--- TradingLib/DatabaseRow.cs
+++ TradingLib/DatabaseRow.cs
@@ -4,18 +4,46 @@
 	/// Holds the data of a single row of the database.
 	/// </summary>
 	public class DatabaseRow
-	{
-		public double date;
-		public double price;
-		public double volBid;
-		public double volAsk;
+    {
+        public double date;
+        public double price;
+        public double bid;
+        public double sizeBid;
+        public double volBid;
+        public double ask;
+        public double sizeAsk;
+        public double volAsk;
+        public double percChange;
+        public double high;
+        public double low;
+        public double volume;
 
-		public DatabaseRow(double date, double price, double volBid, double volAsk)
+		public DatabaseRow(
+		    double date,
+		    double price,
+		    double bid,
+		    double sizeBid,
+            double volBid,
+		    double ask,
+		    double sizeAsk,
+		    double volAsk,
+            double percChange,
+	        double high,
+	        double low,
+	        double volume)
 		{
-			this.date = date;
-			this.price = price;
-			this.volBid = volBid;
-			this.volAsk = volAsk;
+		    this.date = date;
+		    this.price = price;
+		    this.bid = bid;
+		    this.sizeBid = sizeBid;
+		    this.volBid = volBid;
+		    this.ask = ask;
+		    this.sizeAsk = sizeAsk;
+		    this.volAsk = volAsk;
+		    this.percChange = percChange;
+		    this.high = high;
+		    this.low = low;
+		    this.volume = volume;
 		}
 	}
 }
