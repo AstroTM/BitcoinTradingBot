43831fdb62d917586d0e88ae662a85dd3c6ba83c
Matt Beton <matthew.beton@gmail.com>
2017-08-13 18:59:53 +0100

    Sorta fixed InputData.cs
    Need to change DatabaseRow to be an array to make it iterable.

--- NeuralNetwork/InputData.cs
+++ NeuralNetwork/InputData.cs
@@ -8,15 +8,15 @@ namespace TradingBot
 		public double[,] xTrain;
 		public double[,] xTest;
 
-		public int maxDate = 2147483647;
-		public double maxPrice;
-		public double maxVolAsk;
-		public double maxVolBid;
+	    public double[] max;
 
 		public InputData(List<DatabaseRow> inputAsDatabaseRows)
 		{
-			// Makes sure number of inputs is a multiple of 3, so that it splits into arrays neatly.
-			if (inputAsDatabaseRows.Count % 3 == 1) inputAsDatabaseRows.RemoveAt(0);
+            max = new double[11];
+		    max[0] = 2147483647;
+
+            // Makes sure number of inputs is a multiple of 3, so that it splits into arrays neatly.
+            if (inputAsDatabaseRows.Count % 3 == 1) inputAsDatabaseRows.RemoveAt(0);
 			if (inputAsDatabaseRows.Count % 3 == 2)
 			{
 				inputAsDatabaseRows.RemoveAt(0);
@@ -27,18 +27,32 @@ namespace TradingBot
 
 			foreach (DatabaseRow row in inputAsDatabaseRows)
 			{
-				if (row.price > maxPrice) maxPrice = row.price;
-				if (row.volAsk > maxVolAsk) maxVolAsk = row.volAsk;
-				if (row.volBid > maxVolBid) maxVolBid = row.volBid;
-			}
+			    if (row.price >      max[1]) max[1] = row.price;
+			    if (row.bid >        max[2]) max[2] = row.bid;
+			    if (row.sizeBid >    max[3]) max[3] = row.sizeBid;
+			    if (row.volBid >     max[4]) max[4] = row.volBid;
+			    if (row.ask >        max[5]) max[5] = row.ask;
+			    if (row.sizeAsk >    max[6]) max[6] = row.sizeAsk;
+			    if (row.volAsk >     max[7]) max[7] = row.volAsk;
+			    if (row.percChange > max[8]) max[8] = row.percChange;
+			    if (row.high >       max[9]) max[9] = row.high;
+			    if (row.low >        max[10]) max[10] = row.low;
+            }
 
 			for (int i = 0; i < inputAsDatabaseRows.Count; i++)
 			{
-				inputAsDatabaseRows[i].date = inputAsDatabaseRows[i].date / maxDate;
-				inputAsDatabaseRows[i].price = inputAsDatabaseRows[i].price / maxPrice;
-				inputAsDatabaseRows[i].volAsk = inputAsDatabaseRows[i].volAsk / maxVolAsk;
-				inputAsDatabaseRows[i].volBid = inputAsDatabaseRows[i].volBid / maxVolBid;
-			}
+			    inputAsDatabaseRows[i].date = inputAsDatabaseRows[i].date / max[0];
+			    inputAsDatabaseRows[i].price = inputAsDatabaseRows[i].price / max[1];
+			    inputAsDatabaseRows[i].bid = inputAsDatabaseRows[i].bid / max[2];
+			    inputAsDatabaseRows[i].sizeBid = inputAsDatabaseRows[i].sizeBid / max[3];
+			    inputAsDatabaseRows[i].volBid = inputAsDatabaseRows[i].volBid / max[4];
+			    inputAsDatabaseRows[i].ask = inputAsDatabaseRows[i].ask / max[5];
+			    inputAsDatabaseRows[i].sizeAsk = inputAsDatabaseRows[i].sizeAsk / max[6];
+			    inputAsDatabaseRows[i].volAsk = inputAsDatabaseRows[i].volAsk / max[7];
+			    inputAsDatabaseRows[i].percChange = inputAsDatabaseRows[i].percChange / max[8];
+			    inputAsDatabaseRows[i].high = inputAsDatabaseRows[i].high / max[9];
+			    inputAsDatabaseRows[i].low = inputAsDatabaseRows[i].low / max[10];
+            }
 
 			double[,] input = new double[inputAsDatabaseRows.Count, 4];
 			for (int i = 0; i < inputAsDatabaseRows.Count; i++)
@@ -47,6 +61,17 @@ namespace TradingBot
 				input[i, 1] = inputAsDatabaseRows[i].price;
 				input[i, 2] = inputAsDatabaseRows[i].volAsk;
 				input[i, 3] = inputAsDatabaseRows[i].volBid;
+			    inputAsDatabaseRows[i].date = inputAsDatabaseRows[i].date;
+			    inputAsDatabaseRows[i].price = inputAsDatabaseRows[i].price;
+			    inputAsDatabaseRows[i].bid = inputAsDatabaseRows[i].bid;
+			    inputAsDatabaseRows[i].sizeBid = inputAsDatabaseRows[i].sizeBid;
+			    inputAsDatabaseRows[i].volBid = inputAsDatabaseRows[i].volBid;
+			    inputAsDatabaseRows[i].ask = inputAsDatabaseRows[i].ask;
+			    inputAsDatabaseRows[i].sizeAsk = inputAsDatabaseRows[i].sizeAsk;
+			    inputAsDatabaseRows[i].volAsk = inputAsDatabaseRows[i].volAsk;
+			    inputAsDatabaseRows[i].percChange = inputAsDatabaseRows[i].percChange;
+			    inputAsDatabaseRows[i].high = inputAsDatabaseRows[i].high;
+			    inputAsDatabaseRows[i].low = inputAsDatabaseRows[i].low;
 			}
 
 			xTrain = new double[thirdLength * 2, 4];
