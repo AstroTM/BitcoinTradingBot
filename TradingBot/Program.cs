ad6e1654405806fbd2bb1f6022eeabc8bd84e74b
Matt Beton <matthew.beton@gmail.com>
2017-08-14 21:04:26 +0100

    Added weight writing code.

--- TradingBot/Program.cs
+++ TradingBot/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -9,26 +10,54 @@ using Microsoft.Scripting.Hosting;
 
 namespace TradingBot
 {
-	class Program
-	{
-		private static ApiReader APR;
-		private static DatabaseConnector DBC;
+    class Program
+    {
+        private static ApiReader APR;
+        private static DatabaseConnector DBC;
 
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		static void Main(string[] args)
-		{
-			APR = new ApiReader(); // Initialises ApiReader
-			DBC = new DatabaseConnector(); // Initialises DatabaseConnector
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            APR = new ApiReader(); // Initialises ApiReader
+            DBC = new DatabaseConnector(); // Initialises DatabaseConnector
 
-			NeuralNetwork ANN = new NeuralNetwork();
+            NeuralNetwork ANN = new NeuralNetwork();
 
-			List<DatabaseRow> rows = DBC.SelectAllFromDatabase();
+            List<DatabaseRow> rows = DBC.SelectAllFromDatabase();
 
-			ANN.TrainNetwork(rows, 10);
+            ANN.TrainNetwork(rows, 10);
 
-		    Console.ReadLine();
-		}
-	}
+            double[] weights = new double[ANN.Synapses.Count];
+            for (int i = 0; i < ANN.Synapses.Count; i++)
+            {
+                weights[i] = ANN.Synapses[i].Weight;
+            }
+
+            WriteWeights(weights);
+
+            Console.ReadLine();
+        }
+
+        static void WriteWeights(double[] weights)
+        {
+            string WeightSaveString = @"C:\Users\matth\OneDrive\Documents\Visual Studio 2017\Projects\TradingBot\weights.txt";
+
+            if (File.Exists(WeightSaveString))
+            {
+                // Delete file
+                File.Delete(WeightSaveString);
+            }
+
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(WeightSaveString))
+            {
+                foreach (double weight in weights)
+                {
+                    file.WriteLine(weight);
+                }
+            }
+        }
+    }
 }
